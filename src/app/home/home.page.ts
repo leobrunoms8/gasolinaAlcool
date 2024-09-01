ae8f6cfc490990c72c5fb4ab4f0b3cd87743cf5b
Leonardo Soares <leobrunoms8@hotmail.com>
2024-09-01 20:26:20 -0300

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+  
+  public resultado: String = ""
+  precoAlcool: any;
+  precoGasolina: any;
+
+  calcular() {
+
+    //Validar se os campos foram preenchidos
+    if(this.precoAlcool && this.precoGasolina){
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      //Calculo
+
+      var rendimento = pAlcool / pGasolina
+
+      if(rendimento >= 0.7){
+        this.resultado = "Melhor utilizar gasolina"
+      }else{
+        
+        this.resultado = "Melhor utilizar Álcool"
+      }
+    }else{
+      this.resultado = "Preencha corretamente os campos!"
+    }
+    
+  }
+  
 
   constructor() {}
 
